132c7bfa16565d4b63cb69b6396f9170379f20e9
davidvn81 <drvotrgi@gmail.com>
2023-06-06 14:01:25 +0200

    amélioration du menu

--- 05.06.2023/jour5-navigation/src/components/Menu.jsx
+++ 05.06.2023/jour5-navigation/src/components/Menu.jsx
@@ -3,11 +3,27 @@ import {Link} from 'react-router-dom'
 
 function Menu() {
   return (
-    <div className='menu'>
-        <Link to='/'>Accueil</Link>
-        <Link to='/contacter'>Nous contacter</Link>
-        <Link to='/login'>Connexion</Link>
-    </div>
+    <div className='bg-primary'>
+        <nav className='navbar navbar-expand container navbar-dark'>
+          <span className='navbar-brand'>Navigation</span>
+          <ul className='navbar-nav'>
+            <li className='nav-item'>
+              <Link to='/' className='nav-link'>Accueil</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/produits' className='nav-link'>Produits</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/contacter' className='nav-link'>Nous contacter</Link>
+            </li>
+          </ul>
+          <ul className='navbar-nav ms-auto'>
+            <li className='nav-item'>
+              <Link to='/login' className='nav-link'>Connexion</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
   )
 }
 
