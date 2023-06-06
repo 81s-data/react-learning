381a01f84ed051e5ef048321052b35c1f03f1898
davidvn81 <drvotrgi@gmail.com>
2023-06-06 14:29:11 +0200

    Mise à jour des Routes

--- 05.06.2023/jour5-navigation/src/main.jsx
+++ 05.06.2023/jour5-navigation/src/main.jsx
@@ -5,6 +5,8 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Accueil from './pages/Accueil.jsx'
 import NousContacter from './pages/NousContacter.jsx'
 import Login from './pages/Login.jsx'
+import Produits from './pages/Produits.jsx'
+import ProduitSingle from './pages/ProduitSingle.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -13,6 +15,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path='/' element={<App />}>
           <Route index element={<Accueil />}/>
           <Route path='contacter' element={<NousContacter />}/>
+          <Route path='produits/'>
+            <Route index element={<Produits />}/>
+            <Route path=':id/:title' element={<ProduitSingle />}/>
+          </Route>
           <Route path='login' element={<Login />}/>
         </Route>
       </Routes> {/** fin du Router */}
