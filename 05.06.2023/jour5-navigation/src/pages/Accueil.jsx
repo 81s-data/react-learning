3622da6ab051a843cabaefffbafcac838a5981da
davidvn81 <drvotrgi@gmail.com>
2023-06-06 14:28:13 +0200

    Création Page Accueil (test)

--- 05.06.2023/jour5-navigation/src/pages/Accueil.jsx
+++ 05.06.2023/jour5-navigation/src/pages/Accueil.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Accueil() {
+  // requete Ajax
+  // afficher dans le composant
+  // <article>
+  //  <h2>titre</h2>
+  //  tag1 tag2 tag3
+  //  nb commentaire
+  // </article>
+
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/posts')
+    .then(response => response.json())
+    .then(({posts}) => {
+      setPosts(posts);
+    })
+  }, [])
+
   return (
-    <div>Accueil</div>
+    <div className='row'>
+      {posts.map(({title, tags, reactions}, key) => {
+        return <article key={key} className='col-4'>
+          <h2>{title}</h2>
+          {tags.map((item, key) => {
+            return <span key={key} className='badge bg-warning mx-2'>#{item}</span>
+          })}
+          <br />
+          Commentaires: {reactions}
+          <hr />
+        </article>
+      })}
+    </div>
   )
 }
 
