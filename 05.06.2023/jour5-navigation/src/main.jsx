95ab1dd053ef567ddc798dd77820229d1c2fd494
davidvn81 <drvotrgi@gmail.com>
2023-06-07 11:07:14 +0200

    refactor: dashboard route

--- 05.06.2023/jour5-navigation/src/main.jsx
+++ 05.06.2023/jour5-navigation/src/main.jsx
@@ -7,6 +7,7 @@ import NousContacter from './pages/NousContacter.jsx'
 import Login from './pages/Login.jsx'
 import Produits from './pages/Produits.jsx'
 import ProduitSingle from './pages/ProduitSingle.jsx'
+import Dashboard from './pages/Dashboard.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -20,6 +21,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             <Route path=':id/:title' element={<ProduitSingle />}/>
           </Route>
           <Route path='login' element={<Login />}/>
+          <Route path='admin' element={<Dashboard />}/>
         </Route>
       </Routes> {/** fin du Router */}
     </BrowserRouter>
