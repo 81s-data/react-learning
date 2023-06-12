3778daaecc90873de17aacc5045193a11cc6ef33
davidvn81 <drvotrgi@gmail.com>
2023-06-12 12:36:39 +0200

    correction TP1

---
+++ 2023.06.12/TP1-webdev-correction/src/composants/Liste.jsx
@@ -0,0 +1,78 @@
+import React , {useContext , useState } from 'react'
+import {OperationContext} from "../context/OperationContext"
+import { isNumeric } from '../utils'
+
+function Liste() {
+   
+    const {operations , supprimer , update} = useContext(OperationContext)
+    const [id, setId] = useState(0)
+    const [localOperation, setLocalOperation] = useState({})
+
+    function modif(e){
+        const cloneLocalOperation = structuredClone(localOperation);
+        cloneLocalOperation[e.target.name] = e.target.value
+        setLocalOperation(cloneLocalOperation)
+    }   
+
+    function submit(e){
+        e.preventDefault();
+
+        if(localOperation.nom.length < 3 || !isNumeric(localOperation.montant) ){
+        alert("formulaire invalide");
+        return ;
+        } 
+       
+        update(localOperation);
+        setId(0)
+    }
+  return (
+    <div>
+        <h2>Détail des opérations</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>#id</th>
+                    <th>#nom</th>
+                    <th>#montant</th>
+                    <th>#action</th>
+                </tr>
+            </thead>
+            
+                { 
+                    operations.map(function(item , key){
+                        return <tbody key={key}>
+                            { item.id === id 
+                            
+                            ?
+                            <tr>
+                                <td colSpan={4}>
+                                <form className='form-modif' onSubmit={submit}>
+                                    <input type="hidden" name="id" value={localOperation.id} />
+                                    <input type="text" name="nom" value={localOperation.nom} onChange={ modif} />
+                                    <input type="text" name="montant" value={localOperation.montant}  onChange={modif} />
+                                    <input type="submit" value="go" />
+                                </form>
+                                </td>
+                            </tr>
+                            : 
+                            <tr>
+                                <td>{item.id}</td>
+                                <td>{item.nom}</td>
+                                <td>{item.montant}</td>
+                                <td>
+                                    <button className='modifier me-3' onClick={function(){ setId(item.id) ; setLocalOperation(item) }}>modifier</button>
+                                    <button className='supprimer' onClick={function(){ supprimer(item) }}>supprimer</button>
+                                </td>
+                            </tr>
+                            
+                            }
+                         </tbody>
+                    })
+                }
+           
+        </table>
+    </div>
+  )
+}
+
+export default Liste
---
+++ 2023.06.12/TP1-webdev-correction/src/context/OperationContext.jsx
@@ -0,0 +1,72 @@
+import { createContext , useState , useEffect } from "react";
+
+export const OperationContext = createContext();
+
+export function OperationContextProvider ({ children }){
+
+    const [operations,setOperations] = useState([])
+
+    useEffect( function(){
+        fetch("http://localhost:4201/operations")
+            .then(function(reponse){ return reponse.json()})
+            .then(function(data){  setOperations(data) })
+    }, [])
+
+    function add(item){
+        const options = { 
+                method :"POST", 
+                body : JSON.stringify(item),
+                headers : { "content-type" : "application/json"}
+        }
+        fetch("http://localhost:4201/operations" , options )
+            .then(function(reponse){return reponse.json()})
+            .then(function(data){ 
+                const cloneOperation = structuredClone(operations)
+                //item.id = Math.ceil(Math.random() * 100000)
+                cloneOperation.push(data);
+                setOperations (cloneOperation);
+            })
+    }
+
+    function supprimer (ope){
+        // réalise une requête ajax vers la base de données 
+        // requête DELETE pour élément qui a l'id ope.id
+        const options = { method : "DELETE" }
+        fetch(`http://localhost:4201/operations/${ope.id}`, options)
+            .then(function(reponse){ return reponse.json()})
+            .then(function(){
+                // effectué une mis à jour du state operations
+                const cloneOperation = structuredClone(operations)
+                const index = cloneOperation.findIndex(function(item){
+                    return item.id === ope.id
+                });
+                cloneOperation.splice(index, 1); // suppression
+                setOperations(cloneOperation);
+            })
+    }
+
+    function update(item){
+        item.id = Number(item.id)
+        item.montant = Number(item.montant)
+        const options = { 
+            method :"PUT", 
+            body : JSON.stringify(item),
+            headers : { "content-type" : "application/json"}
+    }
+    fetch(`http://localhost:4201/operations/${item.id}` , options )
+        .then(function(reponse){return reponse.json()})
+        .then(function(){ 
+            const cloneOperation = structuredClone(operations)
+            const index = cloneOperation.findIndex(function(ope){
+                return item.id === ope.id
+            });
+            cloneOperation.splice(index, 1, item);
+            setOperations (cloneOperation);
+        })
+    }
+
+    return <OperationContext.Provider value={{operations , add , supprimer , setOperations, update}}>
+        {children}
+    </OperationContext.Provider>
+
+}
