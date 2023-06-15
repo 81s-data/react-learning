586189f8d63e228b6e4079cb0c51a2e8191bbd13
81s <drvotrgi@gmail.com>
2023-06-15 15:55:39 +0200

    unit tests basics

--- 2023.06.14/jour12-test-unitaire/lib.js
+++ 2023.06.14/jour12-test-unitaire/lib.js
@@ -2,4 +2,73 @@ const generateTitle = (date) => {
     return `<h1>article publié le ${new Intl.DateTimeFormat("fr-FR").format(date)}</h1>`
 }
 
+const fizzBuzz = (number) => {
+    let magicWord = '';
+    if (number % 3 === 0) magicWord = 'fizz';
+    if (number % 5 === 0) magicWord += 'buzz';
+    return magicWord;
+}
+
+const getArticle = (tab, id) => {
+    return tab.filter(item => {
+        return item.id === id;
+    })[0]
+}
+
+const priceTaxInclude = (price, vat) => {
+    if (typeof price !== 'number') {
+        throw new Error('Price must be a number');
+    }
+    if (typeof vat !== 'number') {
+        throw new Error('VAT must be a number');
+    }
+    return price * vat;
+}
+
+const anagram = (word1, word2) => {
+    if(word2.length !== word1.length) return false;
+    for(let i = 0; i < word1.length; i++) {
+        if (!word2.toLowerCase().includes(word1[i].toLowerCase())) {
+            return false;
+        }
+    }
+    return true
+}
+
+const convertRomanToArabic = (roman) => {
+    let arabic = 0;
+    const romanNumbers = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    for (let i = 0; i < roman.length; i++) {
+        // TODO implements the translation
+    }
+}
+
+// Cas de test de fonction appelant une autre fonction
+
+const getUser = () => {
+    return {id: 1, nom: 'auteur'}
+}
+
+const getArticleByUser = (getUser) => {
+    return {
+        id : 1 ,
+        titre : "article1",
+        auteur : getUser()
+    }
+}
+
 module.exports.generateTitle = generateTitle;
+module.exports.fizzBuzz = fizzBuzz;
+module.exports.getArticle = getArticle;
+module.exports.priceTaxInclude = priceTaxInclude;
+module.exports.anagram = anagram;
+module.exports.convertRomanToArabic = convertRomanToArabic;
+module.exports.getArticleByUser = getArticleByUser;
--- 2023.06.14/jour12-test-unitaire/test/lib.test.js
+++ 2023.06.14/jour12-test-unitaire/test/lib.test.js
@@ -1,4 +1,10 @@
-const { generateTitle } = require('../lib')
+const { generateTitle,
+        fizzBuzz,
+        getArticle,
+        priceTaxInclude,
+        anagram,
+        convertRomanToArabic,
+        getArticleByUser} = require('../lib')
 
 it('Should return <h1>article publié le 15/06/2023</h1>', () => {
     expect(generateTitle(new Date()))
@@ -6,9 +12,98 @@ it('Should return <h1>article publié le 15/06/2023</h1>', () => {
 })
 
 it('Should return <h1>article publié le 14/06/2023</h1>', () => {
-    let date = new Date;
+    const date = new Date;
     date.setDate(date.getDate() - 1);
     expect(generateTitle(date))
     .toBe('<h1>article publié le 14/06/2023</h1>')
+})
+
+it('fizzBuzz should return correct values', () => {
+    expect(fizzBuzz(8)).toBe('');
+
+    expect(fizzBuzz(3)).toBe('fizz');
+
+    expect(fizzBuzz(5)).toBe('buzz');
+
+    expect(fizzBuzz(15)).toBe('fizzbuzz');
+})
+
+it('getArticle should return the right object', () => {
+
+    const tab = [
+        {id: 0, title: "title1"},
+        {id: 1, title: "title2"},
+        {id: 45, title: "title46"},
+        {id: -234, title: "title"}
+    ];
+
+    const id = -234;
+
+    expect(getArticle(tab, id)).toHaveProperty("id")
+    expect(getArticle(tab, id)).toHaveProperty("title")
+    expect(getArticle(tab, id)).toMatchObject({id: -234, title: "title"})
+    expect(getArticle(tab, id)).not.toBe({id: -234, title: "title"})
+})
+
+describe('priceTaxInclude', function () {
+
+    it('should return VAT price', function() {
+        expect(priceTaxInclude(1,1.4)).toBe(1.4)
+    })
+
+    it('if parameters are not numbers, should throw error', function() {
+        expect(() => priceTaxInclude('1','1.4')).toThrow();
+    })
+})
+
+describe('anagram', function(){
+
+    it('should return true', function() {
+        expect(anagram('Friend', 'finDer')).toBe(true);
+    })
+
+    test("mot avec longueurs différentes", function(){
+        expect(anagram("a" , "ab") ).toBeFalsy()
+    })
+
+    test("mots correctes", function(){
+        const listes = [
+            {
+                mot1 : "finder",
+                mot2 : "Friend",
+            },
+            {
+                mot1 : 'Mary',
+                mot2 : 'Army',
+            }, 
+            {
+                mot1 : 'nectar',
+                mot2 : 'carnet',
+            }, 
+            {
+                mot1 : 'parisien',
+                mot2 : 'aspirine',
+            }
+        ]
+        for(let {mot1, mot2} of listes){
+            expect(anagram(mot1 , mot2) ).toBeTruthy()
+        }
+
+    })
+})
+
+describe('getArticleByUser', function(){
+
+    it('Should return an object with id, titre, auteur and auteur an object with id and nom', function() {
+        const getUserSimule = jest.fn().mockReturnValue({ id : 123 , nom : "azerty" })
+
+    const resultat = getArticleByUser(getUserSimule)
+
+    expect(resultat).toHaveProperty("id")
+    expect(resultat).toHaveProperty("titre")
+    expect(resultat).toHaveProperty("auteur")
+    expect(resultat.auteur).toHaveProperty("id")
+    expect(resultat.auteur).toHaveProperty("nom")
+    })
 })
 
